67e4d8363536e649177c2b335e7cef6816878612
PatrickDominick <pdneary@gmail.com>
2021-09-10 16:09:06 -0500

    built out grid and header navbar components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
+
 class Layout extends Component {
   render() {
     return (
       <div className='layout'>
-        <h1>eCommerce Stickers</h1>
-        
+        {this.props.children} 
+        <Header />
+        <Navbar />       
       </div>
     );
   }
