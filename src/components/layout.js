693cbede6463d5beb6b80dc1fe35a0e8a39a2f5c
PatrickDominick <pdneary@gmail.com>
2021-09-10 15:39:24 -0500

    initial

---
+++ src/components/layout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        <h1>eCommerce Stickers</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default Layout;
