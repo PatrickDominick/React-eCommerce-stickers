daefc128d675af31822c7cd20710e222d3515951
PatrickDominick <pdneary@gmail.com>
2021-09-14 13:00:18 -0500

    built sign in and sign up routes and components

---
+++ src/components/auth/signin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                sign in
+            </div>
+        )
+    }
+}
---
+++ src/components/auth/signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class SignUp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="sign-up">
+                sign up
+            </div>
+        )
+    }
+}
