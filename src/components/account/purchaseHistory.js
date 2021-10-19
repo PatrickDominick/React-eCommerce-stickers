2b2d9323121d14fb6f2974e6b2151b57bc01e87b
PatrickDominick <pdneary@gmail.com>
2021-10-19 14:54:54 -0500

    setup purchase history child component

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 import PageTitle from "../pageTitle";
+import PurchaseDetail from "./purchaseDetail";
+import Purchases from "./purchases";
 
 class PurchaseHistory extends Component {
     render() {
         return (
             <div className= "purchase-history">
-                <Pagetitle className="purchase-history__page-title" title="Purchase History" />
+                <PageTitle className="purchase-history__page-title" title="Purchase History" />
+                <Purchases />
+                <PurchaseDetail className="purchase-history__detail"/>
+                <div className="purcase-history__border-bottom"></div>
             </div>
         )
     }
